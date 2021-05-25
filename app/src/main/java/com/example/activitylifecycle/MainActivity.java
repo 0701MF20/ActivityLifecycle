ee6028dea2f6f12d3ace5b1fc2b25e6b982f5669
0701MF20 <farazmohd0701@gmail.com>
2021-05-25 17:41:01 +0530

    adding the comment

--- app/src/main/java/com/example/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -8,35 +8,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //Start method is executing  just after launching the app
     @Override
     protected void onStart() {
         super.onStart();
         Log.v("MainActivity","Start");
     }
+    //onResume() method is used in order to continue for activity representing in visible
     @Override
     protected void onResume() {
         super.onResume();
         Log.v("MainActivity","Resume");
     }
-
+//onPause() method is actually used to go for partially visible state
     @Override
     protected void onPause() {
         super.onPause();
         Log.v("MainActivity","Pause");
     }
-
+//onStop() method is actually used to hidden state
     @Override
     protected void onStop() {
         super.onStop();
         Log.v("MainActivity","Stop");
     }
-
+//onDestroy() method is used for going in destroy state
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.v("MainActivity","Destroy");
     }
-
+//onRestart() method is used for using to restart from paused state
     @Override
     protected void onRestart() {
         super.onRestart();
